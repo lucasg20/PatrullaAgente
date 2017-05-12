e37c267d2073c35631f478d234f9a2c65a05cb93
rhdperez <rhdperez@gmail.com>
2017-05-11 23:49:34 -0300

    pruebaPatrulla

---
+++ patrulla/src/frsf/cidisi/exercise/patrullero/search/ObjetivoAgente.java
@@ -0,0 +1,20 @@
+
+
+package frsf.cidisi.exercise.patrullero.search;
+
+import frsf.cidisi.faia.agent.search.GoalTest;
+import frsf.cidisi.faia.state.AgentState;
+
+public class ObjetivoAgente extends GoalTest {
+
+    @Override
+    public boolean isGoalState (AgentState agentState) {
+    
+    	// TODO: Complete Method
+        if  (true) //(PosPatrullero=PosIncidente)
+        	{
+            return true;
+        	}
+        return false;
+	}
+}
---
+++ patrulla/src/frsf/cidisi/exercise/patrullero/search/PatrulleroMain.java
@@ -0,0 +1,19 @@
+package frsf.cidisi.exercise.patrullero.search;
+
+import frsf.cidisi.faia.exceptions.PrologConnectorException;
+import frsf.cidisi.faia.simulator.SearchBasedAgentSimulator;
+
+public class PatrulleroMain {
+
+    public static void main(String[] args) throws PrologConnectorException {
+        Patrullero agent = new Patrullero();
+
+        Mapa environment = new Mapa();
+
+        SearchBasedAgentSimulator simulator =
+                new SearchBasedAgentSimulator(environment, agent);
+        
+        simulator.start();
+    }
+
+}
